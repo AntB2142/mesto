9b8e5d4ede91890c4e19a12f10bf60cc0bb51439
Anton Burov <killfire2142@yandex.ru>
2020-11-22 21:14:41 +0300

    версия 1

---
+++ blocks/scripts/index.js
@@ -0,0 +1,28 @@
+let popup = document.querySelector(".popup");
+let editButton = document.querySelector(".profile__edit-button");
+let popupClose = document.querySelector(".popup__close");
+let title = document.querySelector(".profile__title");
+let subtitle = document.querySelector(".profile__subtitle");
+let form = document.querySelector(".popup__form");
+let nameField = document.querySelector(".popup__input_type_name");
+let statusField = document.querySelector(".popup__input_type_status");
+
+function showPopup() {
+    popup.classList.add("popup__opened");
+    nameField.value = title.textContent;
+    statusField.value = subtitle.textContent;
+}
+
+function closePopup() {
+    popup.classList.remove("popup__opened");
+}
+
+function submitForm(event) {
+    event.preventDefault();
+    title.textContent = nameField.value;
+    subtitle.textContent = statusField.value;
+    closePopup();
+}
+editButton.addEventListener("click", showPopup);
+popupClose.addEventListener("click", closePopup);
+form.addEventListener("submit", submitForm);
